71ba69dbbf7d1b513e9e9dc5cd7d67e60cc53d17
unknown <manhdao248205@gmail.com>
2024-05-24 22:17:41 +0700

    lab 3

---
+++ bai 1 lab 3/bai 1 lab 3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai_1_lab_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = "example.txt";
+
+            using (FileStream fs = new FileStream(path, FileMode.Create))
+            {
+                using (StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.UTF8))
+                {
+                    sw.WriteLine("username: myUsername");
+                    sw.WriteLine("password: myPassword");
+                }
+            }
+
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                using (StreamReader sr = new StreamReader(fs, System.Text.Encoding.UTF8))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ bai 2 lab 3/bai 2 lab 3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai_2_lab_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            string username = "myUsername";
+            string password = "myPassword";
+            string time = DateTime.Now.ToString();
+
+            using (StringWriter sw = new StringWriter())
+            {
+                sw.WriteLine("username: " + username);
+                sw.WriteLine("password: " + password);
+                sw.WriteLine("time save: " + time);
+
+                string content = sw.ToString();
+
+                using (StringReader sr = new StringReader(content))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+}   
